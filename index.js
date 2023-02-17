fdf744680ab0d3cd45900611ddc731df73cb53aa
Jose <110537455+JBarthaP@users.noreply.github.com>
2023-02-17 20:54:33 +0100

    finishing projects section

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 function hideorshowNav() {
-    document.getElementById("sidebar").style.width = document.getElementById("sidebar").style.width ==="0px" ? "200px" : "0" 
+   console.log(document.getElementById("sidebar").style.width );
+    document.getElementById("sidebar").style.width = document.getElementById("sidebar").style.width ==="0px" ||  document.getElementById("sidebar").style.width ==="" ? "200px" : "0" 
 }
+
+function checkMenu(){
+     
+     if( window.innerWidth >= "1025") {
+        document.getElementById("header").classList.add("flex-row")
+        document.getElementById("header").classList.remove("flex-column")
+     }
+     else {
+        document.getElementById("header").classList.add("flex-column")
+        document.getElementById("header").classList.remove("flex-row")
+        
+     } 
+}
+
+window.addEventListener('resize', () => checkMenu())
+window.addEventListener('load', () => checkMenu())
